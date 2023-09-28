9a39a516cb7ba939f134b49dc82ed92ae8a3c276
Kanishk <kanishkpvt01@gmail.com>
2023-09-28 23:07:56 +0530

    final commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,6 @@
 'use client'
 import { motion } from "framer-motion"
 import Link from "next/link"
-import { useEffect } from "react"
 
 const Footer = () => {
     return (
@@ -9,7 +8,8 @@ const Footer = () => {
             <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1.2, ease: 'easeIn' }} className='flex justify-center  h-[50vh] sm:h-[120vh] flex-col w-full gap-4 p-4 sm:px-5   '>
                 <p className='font-interLight w-full text-[2rem] lg:text-[6.875rem] md:text-[5em]
           sm:text-[5rem] text-left'>GOT QUESTIONS OR IDEAS BREWING?</p>
-                <motion.button initial={{ backgroundColor: 'black', color: 'white' }} whileHover={{ backgroundColor: 'white', color: 'black', border: '1px solid black' }} className='self-end sm:mr-[10vw]  text-[13px] sm:text-lg  py-2 px-4 sm:px-6 rounded-3xl bg-black text-white'>Drop me a line</motion.button>
+              <Link className="self-end" href='/contact'>
+              <motion.button initial={{ backgroundColor: 'black', color: 'white' }} whileHover={{ backgroundColor: 'white', color: 'black', border: '1px solid black' }} className=' sm:mr-[10vw]  text-[13px] sm:text-lg  py-2 px-4 sm:px-6 rounded-3xl bg-black text-white'>Drop me a line</motion.button></Link> 
             </motion.div>
             <div className='flex flex-row  items-start   text-[7px] sm:text-[15px] p-5'>
                 <p className='basis-1/3  ' >@ 2023 All Rights Reserved</p>
